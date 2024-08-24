e11bcb01ec85240036ef73b8ea8bb073a5778ed9
tonmoy227 <145573783+tonmoy227@users.noreply.github.com>
2024-08-24 11:34:41 +0600

    V 1.00

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+/* -----------------------------------------------------------------------------
+
+
+
+File:           JS Core
+Version:        1.0
+Last change:    00/00/00 
+-------------------------------------------------------------------------------- */
+;(function($) {
+
+	"use strict";
+	const lenis = new Lenis()
+	function raf(time) {
+		lenis.raf(time)
+		requestAnimationFrame(raf)
+	}
+	requestAnimationFrame(raf)
+	/*
+preloader
+====start====
+*/
+	jQuery(window).on('load', function(){
+		jQuery('#preloader').fadeOut('slow',function(){jQuery(this).remove();});
+	});
+
+	/*
+Wow Animation
+====Start====
+*/
+	if($('.wow').length){
+		var wow = new WOW(
+		{
+			boxClass:     'wow',
+			animateClass: 'animated',
+			offset:       0,
+			mobile:       true,
+			live:         true
+		}
+		);
+		wow.init();
+	};
+	$(window).on("load", function () {
+		Splitting();
+	});
+	jQuery('.video_box').magnificPopup({
+		disableOn: 200,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false,
+	});
+	/*
+Background Image
+====start====
+*/
+	$('[data-background]').each(function() {
+		$(this).css('background-image', 'url('+ $(this).attr('data-background') + ')');
+	});
+	
